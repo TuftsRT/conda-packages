2b5ee9ebce2d330f3bdefee2fe4d241191123876
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2026-08-26 14:56:22 -0400

    add metadata helpers to print utils

--- .github/scripts/_print_utils.sh
+++ .github/scripts/_print_utils.sh
@@ -1,5 +1,20 @@
 # shellcheck disable=SC2148
 
+augment_metadata() {
+  local metadata="$1" owner="$2" channel="$3"
+
+  jq --arg owner "$owner" --arg channel "$channel" \
+    '. + {owner: $owner, channel: $channel}' <<< "$metadata"
+}
+
+build_metadata() {
+  local owner="$1" channel="$2" name="$3" version="$4"
+
+  jq -n --arg owner "$owner" --arg channel "$channel" \
+    --arg name "$name" --arg version "$version" \
+    '{owner: $owner, channel: $channel, name: $name, version: $version}'
+}
+
 code_block() {
   echo '```'
   printf '%s\n' "$1"
